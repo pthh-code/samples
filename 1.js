e59181acddc72a95a165188f240fa8c083529c24
Hanh Pham <pham@verssa.com>
2020-04-09 15:52:21 -0500

    jquery lan 2

---
+++ 1.js
@@ -0,0 +1,21 @@
+﻿ $(function(){
+ $(window).scroll(function(){
+ 	if($('body').scrollTop > 200)
+ 	{
+ 		$('.logo').addClass('maucam');
+ 		$('.navbar').addClass('nentrang')
+ 		.removeClass('navbar-dark')
+ 		.addClass('navbar-light');
+ 		
+ 	}
+ 	else
+ 	{
+ 		$('.logo').removeClass('maucam');
+ 		$('.navbar').removeClass('nentrang')
+ 		.addClass('navbar-dark')
+ 		.removeClass('navbar-light'); 
+ 	}
+ 	
+ })
+})  
+ 
